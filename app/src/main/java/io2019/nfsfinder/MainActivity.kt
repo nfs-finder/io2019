2554b496200ed965615a217a43ef225da3f95c54
Piotr Żuber <piotr.a.zuber@gmail.com>
2019-05-05 04:35:37 +0200

    configured permissions and gradle build for google maps

--- app/src/main/java/io2019/nfsfinder/MainActivity.kt
+++ app/src/main/java/io2019/nfsfinder/MainActivity.kt
@@ -1,12 +1,64 @@
 package io2019.nfsfinder
 
+import android.app.Dialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import kotlinx.android.synthetic.main.activity_main.*
+import io2019.nfsfinder.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val LOG_TAG = "MainActivity"
+    val ERROR_DIALOG_REQUEST = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            Log.i(LOG_TAG, "Login button clicked")
+            Toast.makeText(this, "Podaj dane logowania", Toast.LENGTH_SHORT).show()
+
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
+        }
+
+        if (isServiceOK()) {
+            init()
+        }
+    }
+
+    private fun init() : Unit {
+        btnMapiczka.setOnClickListener {
+            Log.i(LOG_TAG, "Idę do mapiczki")
+
+            val intentMapiczka = Intent(this, MapsActivity::class.java)
+            startActivity(intentMapiczka)
+        }
+    }
+
+    private fun isServiceOK() : Boolean {
+        Log.d(LOG_TAG, "isServiceOK: checking google service version")
+
+        val available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        when {
+            available == ConnectionResult.SUCCESS -> {
+                Log.d(LOG_TAG, "isServiceOK: Google Play Services working")
+                return true
+            }
+            GoogleApiAvailability.getInstance().isUserResolvableError(available) -> {
+                Log.d(LOG_TAG, "isServiceOK: an resolvable error occured")
+                val dialog = GoogleApiAvailability.getInstance().getErrorDialog(this, available, ERROR_DIALOG_REQUEST)
+                dialog.show()
+            }
+            else -> Toast.makeText(this, "You can't take map requests", Toast.LENGTH_SHORT).show()
+        }
+
+        return false
     }
 }
--- app/src/main/java/io2019/nfsfinder/MapsActivity.kt
+++ app/src/main/java/io2019/nfsfinder/MapsActivity.kt
@@ -3,20 +3,39 @@ package io2019.nfsfinder
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val LOG_TAG = "MapsActivity"
+    private val DEFAULT_MAP_ZOOM = 10f
+
+    private val FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
+    private val COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION
+    private val LOCATION_PERM_REQUEST_CODE = 1234
+
+    private var mLocationPermsGranted = false
     private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        getLocationPermission()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -33,11 +52,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show()
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        if (mLocationPermsGranted) {
+            showDeviceLocation()
+
+            if (ActivityCompat.checkSelfPermission(this, FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+
+            mMap.isMyLocationEnabled = true
+            mMap.uiSettings.isMyLocationButtonEnabled = false
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        mLocationPermsGranted = false
+
+        if (requestCode == LOCATION_PERM_REQUEST_CODE) {
+            if (grantResults.isNotEmpty()) {
+                for (item in grantResults) {
+                    if (item != PackageManager.PERMISSION_GRANTED) {
+                        mLocationPermsGranted = false
+                        return
+                    }
+                }
+                mLocationPermsGranted = true
+            }
+        }
+    }
+
+    private fun showDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        try {
+            if (mLocationPermsGranted) {
+                val location = mFusedLocationProviderClient.lastLocation
+                location.addOnCompleteListener {task ->
+                    if (task.isSuccessful) {
+                        val currentLocation: Location = task.result
+
+                        moveCamera(LatLng(currentLocation.latitude, currentLocation.longitude), DEFAULT_MAP_ZOOM)
+                    } else {
+                        Toast.makeText(this, "Unable to get current location", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e(LOG_TAG, "showDeviceLocation: SecurityException: " + e.message)
+        }
+    }
+
+    private fun moveCamera(latLng: LatLng, zoom: Float) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
+    private fun getLocationPermission() {
+        val permissions = arrayOf(FINE_LOCATION, COARSE_LOCATION)
+
+        if (ContextCompat.checkSelfPermission(this.applicationContext, FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.applicationContext, COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermsGranted = true
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERM_REQUEST_CODE)
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERM_REQUEST_CODE)
+        }
     }
 }
