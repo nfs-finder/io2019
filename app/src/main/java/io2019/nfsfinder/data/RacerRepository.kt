d7ed32eb7389511a0567c9b04467aa8f4445d0b3
Jan Kociniak <jk394348@students.mimuw.edu.pl>
2019-05-12 21:17:41 +0200

    changes in RacerRepository

--- app/src/main/java/io2019/nfsfinder/data/RacerRepository.kt
+++ app/src/main/java/io2019/nfsfinder/data/RacerRepository.kt
@@ -1,6 +1,34 @@
 package io2019.nfsfinder.data
 
+import io2019.nfsfinder.data.database.RequestHandler
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
+
 class RacerRepository {
-    val racerMap: Map<Int, Racer> = HashMap()
-    private var refreshTime: Int = 3000 //frequency of updates in milliseconds
+    val racerMap: MutableMap<Int, Racer> = HashMap()
+    private var refreshTime: Long = 3000 //frequency of updates in milliseconds
+    private var searchRadius: Long = 1000 //in meters
+    val requestHandler = RequestHandler()
+
+    init {
+        val updateTask = fixedRateTimer(period = refreshTime) {
+            this@RacerRepository.updateRacerMap()
+        }
+    }
+
+    fun updateRacerMap() {
+        val updateMap: (Set<Racer>) -> Unit = {
+            for (racer in it) {
+                if (racerMap.containsKey(racer.userId)) {
+                    racerMap.getValue(racer.userId).username = racer.username
+                    racerMap.getValue(racer.userId).car = racer.car
+                    racerMap.getValue(racer.userId).location = racer.location
+                } else {
+                    racerMap[racer.userId] = racer
+                }
+            }
+        }
+
+        requestHandler.getRacers(searchRadius, updateMap)
+    }
 }
